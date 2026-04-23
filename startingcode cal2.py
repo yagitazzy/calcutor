3622c49e0c99fd6492d679b6bee339d760c70da3
IanMcPherson11 <Ian16McP@gmail.com>
2026-04-23 10:35:39 -0500

    Update startingcode cal2.py
    Actually allowed the user to choose the operation and for all the methods about the operation get called

--- startingcode cal2.py
+++ startingcode cal2.py
@@ -1,7 +1,6 @@
-
-
 #### TASK 1 - ENTER YOUR TEAM NAME AND NUMBER
-team_name = ""
+import math
+team_name = "Ben_Shamia_Ian"
 
 #### TASK 2 - Clone the empty gitHub repo in your local computer (Member #1)
 
@@ -24,35 +23,50 @@ def calculator():
   print("7. Exponent")
 
   choice = int(input("Enter your choice: "))
-if (choice == 1):
-  print("what is the first number you want to add?") 
-  num1 = int(input("what is the first number you want to add: "))
-  num2 = int(input("what is the second number you want to add: "))
-  addition(num1, num2) 
-elif (choice == 2):
-  num1 = int(input("what is the first number you want to subtract: "))
-  num2 = int(input("what is the second number you want to subtract: "))
-  div(num1, num2)
-elif (choice == 3):
-  num1 = int(input("what is the first number you want to multiply: "))
-  num2 = int(input("what is the second number you want to multiply: "))
-  mult(num1, num2)
-
-
-
-calculator()
+  if (choice == 1):
+      print("what is the first number you want to add?") 
+      num1 = int(input("what is the first number you want to add: "))
+      num2 = int(input("what is the second number you want to add: "))
+      print(f"Sum of your numbers is {addition(num1, num2)}") 
+  elif (choice == 2):
+      num1 = int(input("what is the first number you want to subtract: "))
+      num2 = int(input("what is the second number you want to subtract: "))
+      print(f"Difference of your numbers is {sub(num1, num2)}")
+  elif (choice == 3):
+      num1 = int(input("what is the first number you want to multiply: "))
+      num2 = int(input("what is the second number you want to multiply: "))
+      print(f"Multiplication of your numbers is {mult(num1, num2)}")
+  elif (choice == 4):
+      num1 = int(input("what is the first number you want to divide: "))
+      num2 = int(input("what is the second number you want to divide: "))
+      print(f"Division of your numbers is {div(num1, num2)}")
+  elif (choice == 5):
+      num1 = int(input("what is the first number you want to integer divide: "))
+      num2 = int(input("what is the second number you want to integer divide: "))
+      print(f"Integer Division of your numbers is {intdiv(num1, num2)}")
+  elif (choice == 6):
+      num1 = int(input("what is the number you'd like the square root of: "))
+      print(f"Square Root of your number is {sqrt(num1)}")
+  elif (choice == 7):
+      num1 = int(input("what is the base: "))
+      num2 = int(input("What is it to the power of: "))
+      print(f"Power of your numbers is {mult(num1, num2)}")
 def sub(num1, num2):
-  print("The square root of " + str(number1) + " is " + str(num**0.5))
   return num1 - num2
 
 def sqrt(num):
-  print("The square root of " + str(number1) + " is " + str(num**0.5))
   return num**.5
 
 def div(num1, num2):
-  print("we are  dividing"+ str(num1) + " and " + str(num2) )
   return num1 / num2
-  
+
+def intdiv(num1, num2):
+    return num1 // num2
+
+
 def mult(num1, num2):
-  print("we are  multiplying" + str(number1) + " and " + str(number2) )
   return num1 * num2
+
+def exp(num1, num2):
+    return math.pow(num1, num2) 
+calculator()
